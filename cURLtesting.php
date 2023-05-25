803afddb5af621c5af7889f2af83837c29260e88
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-05-25 13:11:37 +0500

    cURL testing

---
+++ cURLtesting.php
@@ -0,0 +1,26 @@
+<?php
+
+
+// $ch - curl_init();
+// curl_setopt(
+//     $ch,
+//     CURLOPT_URL,
+//     "http://vishal.webhostingindia.racing/php_scripts/curl/index.php?name=vishal"
+// );
+// curl_init(); for initializing url.
+//curl_setopt($ch,"https://www.google.com"); for hit url example :
+//curl exec(); for execute
+//curl close(); for close curl
+
+
+// $curl = curl_init();
+// curl_setopt_array($curl, array(
+//     CURLOPT_URL => $url,
+//     CURLOPT_RETURNTRANSFER => true,
+//     CURLOPT_ENCODING => '',
+//     CURLOPT_MAXREDIRS => 10,
+//     CURLOPT_TIMEOUT => 0,
+//     CURLOPT_FOLLOWLOCATION => true,
+//     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//     CURLOPT_CUSTOMREQUEST => 'GET',
+// ));
