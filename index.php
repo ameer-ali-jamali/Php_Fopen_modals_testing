781c4121859d2bfaad240b95a4defa1e88a6da11
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-05-25 11:56:33 +0500

    fgets done

--- index.php
+++ index.php
@@ -1,25 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
 
-<body>
+    <body>
 
-    <?php
+        <?php
     // $getData = file_get_contents('order.txt');
+
+    // $fileName = "order.txt";
+    // $file = fopen($fileName, 'r');
+    // $getData = fread($file, filesize($fileName));
+    // fclose($file);
+    // echo $getData;
+
+
+
     $fileName = "order.txt";
-    // $new = "this is dumy data for testing";
     $file = fopen($fileName, 'r');
-    $getData = fread($file, filesize($fileName));
+    $getData = fgets($file);
+    // fclose($file);
+    while ($getData = fgets($file)) {
+        echo $getData;
+    }
+
+
+
+    // $new = "this is dumy data for testing";
     // $fwrite = fwrite($file, $new,filesize($fileName));
-    fclose($file);
-    echo $getData;
+
+
+
+
+
+
     ?>
-</body>
+    </body>
 
 </html>
