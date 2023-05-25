77c4def1546454546d89acd572ae559c365c6000
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-05-25 11:52:42 +0500

    fread done

--- index.php
+++ index.php
@@ -11,8 +11,13 @@
 <body>
 
     <?php
-
-    $getData = file_get_contents('order.txt', 'w');
+    // $getData = file_get_contents('order.txt');
+    $fileName = "order.txt";
+    // $new = "this is dumy data for testing";
+    $file = fopen($fileName, 'r');
+    $getData = fread($file, filesize($fileName));
+    // $fwrite = fwrite($file, $new,filesize($fileName));
+    fclose($file);
     echo $getData;
     ?>
 </body>
